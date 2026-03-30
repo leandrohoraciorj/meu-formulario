d19d088a5f9a1c5b992a48db79be254aeeba681c
leandrohoraciorj <111650931+leandrohoraciorj@users.noreply.github.com>
2026-03-30 18:50:52 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Meu sistema online 🚀"
+    return render_template('form.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    nome = request.form['nome'].upper()
+    telefone = request.form['telefone']
+    cidade = request.form['cidade'].upper()
+    bairro = request.form['bairro'].upper()
+
+    print(nome, telefone, cidade, bairro)  # depois vamos salvar no sheet
+
+    return render_template('form.html', sucesso=True)
 
 if __name__ == '__main__':
     app.run()
